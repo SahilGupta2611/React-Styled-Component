6df333626395a9b9ee0c726dc3e5c2f70049fc30
sahilgupta2611 <sahilgupta0902@gmail.com>
2022-11-18 00:16:49 +0530

    initial commits

---
+++ src/App.styled.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+
+export const Container = styled.div`
+width: 100%;
+position: relative;
+min-height: 100vh;
+box-sizing: border-box;
+display: flex;  
+align-items: center;
+flex-direction: row;
+padding: 80px  40px;
+height: 100%;
+border: 2px solid black;
+justify-content : space-around;
+background-color:crimson;
+` 
+export const Card = styled.div`
+border: 5px solid black;
+width: 200px;
+height: 200px;
+background-color: white; 
+box-shadow: 10px 10px 2px 3px;
+`
---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { useState } from "react";
+import { Card, Container } from "./App.styled";
+import "./App.styled.tsx";
+
+function App({}) {
+
+ 
+return (
+    <Container>
+       <Card>Sahil Gupta</Card>
+      <Card>Raja Gupta</Card>
+      <Card>Shubham Gupta</Card> 
+    
+    </Container>
+  );
+}
+
+export default App;
